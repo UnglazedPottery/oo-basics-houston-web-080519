9bfca4b553fef50f6c4203ff4b96477ca57b31e1
Jade Huynh <35jadeh@gmail.com>
2019-08-09 13:51:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :condition, :material
+  attr_reader :brand
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def turn_page
+    puts "Your shoe is as good as new!"
+  end
+ 
+end
